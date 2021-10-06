8c458dd4f3666e903d53f4a26b79d5804c3e92d7
Kelly Solano <kelly.solano.mt@correo.usa.edu.co>
2021-10-05 22:18:45 -0500

    Creacion proyecto spring boot g22

---
+++ src/test/java/com/ciclo3g22/Grupo22/Grupo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3g22.Grupo22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
