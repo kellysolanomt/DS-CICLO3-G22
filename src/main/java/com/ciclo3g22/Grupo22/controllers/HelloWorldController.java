917fe203ed8d283b65644087deb4669b982150d5
Kelly Solano <kelly.solano.mt@correo.usa.edu.co>
2021-10-05 22:52:14 -0500

    Se agrega el metodo GET

---
+++ src/main/java/com/ciclo3g22/Grupo22/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.ciclo3g22.Grupo22.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HelloWorldController {
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Hola mundo";
+    }
+}
